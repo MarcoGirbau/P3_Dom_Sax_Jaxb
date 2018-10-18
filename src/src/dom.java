95a793b25c152297d711cf17d4beb183fd7e5539
xp <xp@CETYSDAM015>
2018-10-18 20:58:52 +0200

    Hemos avanzado

--- src/src/dom.java
+++ src/src/dom.java
@@ -6,10 +6,11 @@
 package src;
 
 import java.io.File;
-import javax.swing.text.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Document;
 
 /**
  *
@@ -35,5 +36,53 @@ public class dom
             e.printStackTrace();
             return -1;
         }
+    }
+    
+    public String recorrerdomymostrar()
+    {
+        String[] datos_nodos = null;
+        String salida = "";
+        Node node;
+        
+        Node raiz = doc.getFirstChild();//Obtenemos el primer nodo
+        NodeList nodelist = raiz.getChildNodes();//Obtenemos una lista de nodos con todos los nodos del hijo raiz
+        
+        for(int i = 0; i < nodelist.getLength(); i++)//Procesamos todos los nodos hijos
+        {
+            node = nodelist.item(0);
+            
+            if(node.getNodeType() == Node.ELEMENT_NODE)
+            {
+                datos_nodos = procesarLibro(node);//nodo libro
+                
+                salida = salida + "\n" + "Publicado en " + datos_nodos[0];
+                salida = salida + "\n" + "El autor es " + datos_nodos[2];
+                salida = salida + "\n" + "El titulo es " + datos_nodos[1];
+                salida = salida + "\n -----------";
+            }
+        }
+        return salida;
+    }
+    
+    public String[] procesarLibro(Node n)
+    {
+        String[] datos = new String[3];
+        Node ntemp = null;
+        int contador = 1;
+        
+        datos[0] = n.getAttributes().item(0).getNodeValue();//Obtenemos el valor del primer atributo del nodo
+        NodeList nodos = n.getChildNodes();//Obtenemos los hijos del libro 
+        
+        for(int i = 0; i < nodos.getLength(); i++)
+        {
+            ntemp = nodos.item(i);
+            if(ntemp.getNodeType() == Node.ELEMENT_NODE)
+            {
+                //Para obtener el texto con el titulo y autor se accede al nodo TEXT hijo de ntemp y se saca su valor 
+                datos[contador] = ntemp.getChildNodes().item(0).getNodeValue();
+                contador++;
+            }
+        }
+        return datos;
     }
 }
