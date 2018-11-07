b073713d07747b490b8f27fd2c9f6ba8badc0258
xp <xp@CETYSDAM015>
2018-11-07 20:58:55 +0100

    jaxb empezado

--- src/src/jaxb.java
+++ src/src/jaxb.java
@@ -5,10 +5,35 @@
  */
 package src;
 
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import jaxb.Libros;
+
 /**
- *
- * @author xp
+ * @author Marco Girbau
  */
-public class jaxb {
+public class jaxb 
+{
+    Libros misLibros;
     
+    public int abrir_XML_JAXB(File fichero) 
+    {
+        JAXBContext contexto;
+        try 
+        {
+            //Crea una instancia JAXB
+            contexto = JAXBContext.newInstance(Libros.class);
+            //Crea un objeto Unmarsheller.
+            Unmarshaller u = contexto.createUnmarshaller();
+            //Deserializa (unmarshal) el fichero
+            misLibros = (Libros) u.unmarshal(fichero);
+            return 0;
+        } 
+        catch (Exception ex) 
+        {
+            ex.printStackTrace();
+            return -1;
+        }
+    }
 }
