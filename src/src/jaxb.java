202095278126bd1d0b71373e9c3a59ff18dbc4b7
xp <xp@CETYSDAM015>
2018-11-14 20:13:44 +0100

    Añadidos comentarios de cosas que faltan por hacer

--- src/src/jaxb.java
+++ src/src/jaxb.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Que salga lo de la editorial de caca de pedo.
  */
 package src;
 
--- src/src/sax.java
+++ src/src/sax.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Cuando encuentre la etiqueta libros que imprima "Estos son los libros del documento seleccionado:".
  */
 package src;
 
